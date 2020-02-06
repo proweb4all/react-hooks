873b534069638d809aae137019cc526b5dd1387e
proweb4all <proweb4all@narod.ru>
2020-02-06 14:39:11 +0300

    begin

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Navbar = () => (
+  <nav className='navbar navbar-dark bg-primary navbar-expand-lg'>
+    <div className='navbar-brand'>
+      Github Поиск
+    </div>
+    <ul className='navbar-nav ta-left'>
+      <li className='nav-item'>
+        <a href='' className='nav-link'>Главная</a>
+      </li>
+      <li className='nav-item'>
+        <a href='' className='nav-link'>Информация</a>
+      </li>
+    </ul>
+  </nav>
+)
