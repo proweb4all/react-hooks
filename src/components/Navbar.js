b7313de284b7dd84f8557a6d7809b32a44c5fd5a
ProWeb4All <proweb4all@narod.ru>
2020-02-10 22:16:37 +0300

    Router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => (
-  <nav className='navbar navbar-dark bg-primary navbar-expand-lg'>
+  <nav className='navbar navbar-dark bg-primary navbar-expand-lg justify-content-between'>
     <div className='navbar-brand'>
       Github Поиск
     </div>
     <ul className='navbar-nav ta-left'>
       <li className='nav-item'>
-        <a href='' className='nav-link'>Главная</a>
+        <NavLink exact to='/' className='nav-link'>Главная</NavLink>
       </li>
       <li className='nav-item'>
-        <a href='' className='nav-link'>Информация</a>
+        <NavLink to='/about' className='nav-link'>Информация</NavLink>
       </li>
+      {/* <li className='nav-item'>
+        <NavLink to='/Profile/:name' className='nav-link'>Профиль</NavLink>
+      </li> */}
     </ul>
   </nav>
 )
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
-    return (
-        <div>
-            <h1>About page</h1>
-        </div>
-    )
-}
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Информация</h1>
+        <p className="lead">Версия приложения: <strong>1.0.0</strong></p>
+      </div>
+    </div>
+  );
+};
