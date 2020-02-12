bf5ef7926cb64a8b77120bc253b35f740f08b6e8
ProWeb4All <proweb4all@narod.ru>
2020-02-12 22:15:52 +0300

    queries

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,15 @@
 import React, { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { GithubContext } from '../context/github/githubContext'
 
 export const Search = () => {
   const [value, setValue] = useState('')
   const {show, hide} = useContext(AlertContext)
+  const github = useContext(GithubContext)
   const onSubmit = (e) => {
     if (e.key !== 'Enter') { return }
     if (value.trim()) {
-      console.log(value)
+      github.search(value.trim())
       hide()
     } else {
       show('Введите ник пользователя!')
